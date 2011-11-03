2fe7f06ddb6cc23a709af0eb03be6c59b270c3f3
Ramon Marco L. Navarro <ramonmaruko@gmail.com>
2011-11-03 14:06:09 +0800

    Redirect logger output to file during testing
    Test logs are stored inside the spec/logs directory. The log files
    have a filename having the format of 'test-#{rspec pid}.log".
    
    Change-Id: I5e4ae779579e401dd62f82fa33a78cf500fb0b74

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,3 +6,14 @@ require 'bundler/setup'
 require 'tempfile'
 
 require 'palmade/puppet_master'
+
+log_dir = File.join(File.expand_path('..', __FILE__), 'logs')
+Dir.mkdir(log_dir) unless Dir.exists? log_dir
+log_file = File.join(log_dir, "test-#{$$}.log")
+logger = Logger.new(log_file)
+
+RSpec.configure do |config|
+  config.before(:each) do
+    Logger.stub(:new) { logger }
+  end
+end
